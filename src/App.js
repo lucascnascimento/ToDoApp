f17a584cdc10dd067e727728a32935d92c110888
Lucas Cordeiro <lucascnascimento@gmail.com>
2019-11-12 13:48:22 -0300

    Created  CreateTodo state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Navbar from './Components/Layout/Navbar'
+import CreateTodo from './Components/Todos/CreateTodo'
 
 const App = () => {
   return (
     <Router>
       <div className="App">
           <Navbar title='ToDo App' icon='fas fa-calendar-check'/>
+          <CreateTodo />
       </div>
     </Router>
   );
