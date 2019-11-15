d0efd41981d47866fc9b2d54006315eed403ad60
Lucas Cordeiro <lucascnascimento@gmail.com>
2019-11-15 10:00:37 -0300

    The create todo has been created using hooks.
    The internal state is updating correctly.
    
    Issue: cant dispatch the todo to the  TodoState

--- src/Components/Todos/CreateTodo.js
+++ src/Components/Todos/CreateTodo.js
@@ -1,6 +1,9 @@
 import React, {useState, useContext} from 'react'
+import TodoContext from '../../Context/Todo/todoContext'
 
 const CreateTodo = () => {
+
+    const todoContext = useContext(TodoContext)
 
     const [project, setProject] = useState(' ')
     const [priority, setPriority] = useState(' ')
@@ -11,10 +14,73 @@ const CreateTodo = () => {
     const [due, setDue] = useState(null)
     const [isComplete, setIsComplete] = useState(false)
     const [id, setId] = useState(' ')
+    const [isCreateTodoActive, setIsCreateTodoActive] = useState(false)
+
+    // TODO: replace the useState for an object
+    const [todo, setTodo] = useState({
+        todoObj: {
+        project: ' ',
+        priority: ' ',
+        context: ' ',
+        creationDate: null, // date
+        completionDate: null, // date
+        description: ' ',
+        due: null, // date
+        isComplete: false,
+        id: ' ',
+        }
+    })
 
 
     const onChange = (e) => {
+        const [project, priority] = e.target.name;
         e.preventDefault();
+        console.log(e.target)
+        switch (e.target.name) {
+            case 'project':
+                setProject(e.target.value)
+                break;
+            case 'priority':
+                setPriority(e.target.value)
+                break;
+            case 'context':
+                setContext(e.target.value)
+                break;
+            case 'creationDate':
+                setCreationDate(e.target.value)
+                break;
+            // case 'completionDate':
+            //     setCompletionDate(e.target.value)
+            //     break;
+            case 'description':
+                setDescription(e.target.value)
+                break;
+            case 'due':
+                setDue(e.target.value)
+                break;
+            // case 'isComplete':
+            //     setIsComplete(e.target.value)
+            //     break;
+            default:
+                break;
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const todoItem =  {
+            project: project,
+            priority: priority,
+            context: context,
+            creationDate: null,
+            completionDate: null,
+            description: description,
+            due: null,
+            isComplete: false,
+            id: ' ',
+        }
+        console.log(todoItem)
+        // todoContext.addTodo(todoItem)
     }
 
     return (
@@ -22,8 +88,14 @@ const CreateTodo = () => {
             <button>
                 Open new todo
             </button>
-            <form>
-                <input type='text' name='project' placeholder='Context' ></input>
+            <form onSubmit={onSubmit}>
+                <input type='text' name='priority' placeholder='Priority' onChange={onChange} required></input>
+                <input type='text' name='creationDate' placeholder='Creation Date' onChange={onChange}></input>
+                <input type='text' name='due' placeholder='Due' onChange={onChange}></input>
+                <input type='text' name='project' placeholder='Project' onChange={onChange} required></input>
+                <input type='text' name='context' placeholder='Context' onChange={onChange}></input>
+                <input type='text' name='description' placeholder='Description' onChange={onChange}></input>
+                <input type='submit' value='Save'></input>
             </form>
         </div>
     )
