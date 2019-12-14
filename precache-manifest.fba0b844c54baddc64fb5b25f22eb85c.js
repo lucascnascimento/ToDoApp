a9ba9a283bb53226ce442e55c0bc28e31a9e3596
Lucas Cordeiro <lucascnascimento@gmail.com>
2019-12-13 21:11:34 -0300

    Updates

---
+++ precache-manifest.fba0b844c54baddc64fb5b25f22eb85c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9d131bac2494f37e8458db78d3d9b7",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "73831428aa274586f340",
+    "url": "/ToDoApp/static/css/2.5a2f6b34.chunk.css"
+  },
+  {
+    "revision": "b64409689aae412a2b17",
+    "url": "/ToDoApp/static/css/main.a94a0423.chunk.css"
+  },
+  {
+    "revision": "73831428aa274586f340",
+    "url": "/ToDoApp/static/js/2.092dac6b.chunk.js"
+  },
+  {
+    "revision": "b64409689aae412a2b17",
+    "url": "/ToDoApp/static/js/main.f22323a3.chunk.js"
+  },
+  {
+    "revision": "89d5d49c96cde81de371",
+    "url": "/ToDoApp/static/js/runtime-main.faf084b2.js"
+  }
+]);
--- src/Context/types.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = 'ADD_TODO'
-export const DELETE_TODO = 'DELETE_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO'
-export const SET_ALERT = 'SET_ALERT'
-export const REMOVE_ALERT = 'REMOVE_ALERT'
